8c68abd94adc10f3abf4d262b866da673032c70a
HamTheHacker <hamzahfawzy9@gmail.com>
2025-09-07 15:32:32 +0300

    Update kalisetup.sh

--- kalisetup.sh
+++ kalisetup.sh
@@ -7,96 +7,91 @@ sudo apt install impacket-scripts ffuf wget -y
 sudo apt install ruby ruby-dev -y
 sudo gem install evil-winrm
 
-
+################## cloning main repo
 cd ~
-
-mkdir kali
-cd kali
-
 git clone https://github.com/HamTheHacker/MyKaliBox.git
+
+################## setting up transfer
 mkdir transfer
+mkdir transfer/powerup
+mv MyKaliBox/tools/* transfer/
+mv transfer/powersploit/common_scripts/* transfer/powerup
+rm -rf transfer/powersploit
+rm -rf transfer/others
 
-cd transfer
-mkdir powerup
-
-cd powerup
-mv ../../MyKaliBox/tools/powersploit/common_scripts/* .
-rm -rf common_scripts
-cd ..
-
-mv ../MyKaliBox/tools/* .
-rm -rf ../MyKaliBox/tools/others
-
-mkdir dirtycow
-cd dirtycow
+mkdir transfer/dirtycow
+cd transfer/dirtycow
 git clone https://gist.github.com/KrE80r/42f8629577db95782d5e4f609f437a54
 mv 42f8629577db95782d5e4f609f437a54/* .
 rm -rf 42f8629577db95782d5e4f609f437a54
 gcc -pthread c0w.c -o compiled_cow -lcrypt
-cd ..
+cd ~
 
-
-
-mkdir windows_suggester
-cd windows_suggester
+mkdir transfer/windows_suggester
+cd transfer/windows_suggester
 git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester.git
 mv Windows-Exploit-Suggester/windows-exploit-suggester.py .
 ./windows-exploit-suggester.py --update
 rm -rf Windows-Exploit-Suggester
-cd ..
+cd ~
 
-mkdir print_nightmare
-cd print_nightmare
+mkdir transfer/print_nightmare
+cd transfer/print_nightmare
 git clone https://github.com/nemo-wq/PrintNightmare-CVE-2021-34527
 mv PrintNightmare-CVE-2021-34527/CVE-2021-34527.py .
 rm -rf PrintNightmare-CVE-2021-34527
-cd ..
+cd ~
 
-cd ld_preload_privesc
+cd transfer/ld_preload_privesc
 gcc -m32 -fPIC -shared -o compiled_32bit_ld_preload.so ld_preload.c -nostartfiles
 gcc -fPIC -shared -o compiled_64bit_ld_preload.so ld_preload.c -nostartfiles
-cd ..
+cd ~
 
+cd transfer
 git clone https://github.com/saleemrashid/sudo-cve-2019-18634.git
 mv sudo-cve-2019-18634 sudoPrivEsc-cve-2019-18634
 cd sudoPrivEsc-cve-2019-18634
 gcc -o exploit.c compiled_exploit
-cd ..
+cd ~
 
-mkdir drupal7
-cd Drupal7
+mkdir transfer/drupal7
+cd transfer/Drupal7
 git clone https://github.com/pimps/CVE-2018-7600
 mv CVE-2018-7600/drupa7-CVE-2018-7600.py .
 mv drupa7-CVE-2018-7600.py drupal7.py
 rm -rf CVE-2018-7600
 pip3 install requests
 pip3 install bs4
-cd ..
+cd ~
 
-cd ..
+
+################## workspace setup
 
 mkdir workspace
-cd workspace
-mkdir findings 
+mkdir workspace/findings 
 
-mv ../MyKaliBox/script/* .
+mv MyKaliBox/script/* workspace/findings
+cd workspace/findings
 sudo chmod +x *
-cd ..
+cd ~
 
+############## krakin setup
 mv MyKaliBox/krakin .
+
+############ remove kali box github repo
 rm -rf MyKaliBox
+# check wut tf this is first: rm -rf transfer/sudo-cve2019-18634
 
-rm -rf transfer/others
-rm -rf transfer/powersploit
-rm -rf transfer/sudo-cve2019-18634
-
-cp workspace/transfer.sh transfer/powerup
-cp workspace/transfer.sh transfer/dirtycow
-cp workspace/transfer.sh transfer/print_nightmare
-cp workspace/transfer.sh transfer/ld_preload_privesc
-cp workspace/transfer.sh transfer/sudoPrivEsc-cve-2019-18634
-cp workspace/transfer.sh transfer/drupal7
-cp workspace/transfer.sh transfer/akagi
-cp workspace/transfer.sh transfer/linpeas
-cp workspace/transfer.sh transfer/spip_exploit/reverse_shell
-cp workspace/transfer.sh transfer/spip_exploit/injection
+############# copy transfer.sh
+cp workspace/findings/transfer.sh transfer/powerup
+cp workspace/findings/transfer.sh transfer/dirtycow
+cp workspace/findings/transfer.sh transfer/print_nightmare
+cp workspace/findings/transfer.sh transfer/ld_preload_privesc
+cp workspace/findings/transfer.sh transfer/sudoPrivEsc-cve-2019-18634
+cp workspace/findings/transfer.sh transfer/drupal7
+cp workspace/findings/transfer.sh transfer/akagi
+cp workspace/findings/transfer.sh transfer/linpeas
+cp workspace/findings/transfer.sh transfer/spip_exploit/reverse_shell
+cp workspace/findings/transfer.sh transfer/spip_exploit/injection
+cp workspace/findings/transfer.sh transfer/php-reverseshell
+cp workspace/findings/transfer.sh transfer/
